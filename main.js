a58a391524f29567e175b6510a6ac70f6f7acf03
erane <eranyo@mta.ac.il>
2023-10-21 16:49:27 +0300

    new data

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-print("hello");
+const express = require("express");
+const customersRouter = require("./routers/customers");
+
+const cors = require("cors");
+
+const app = express();
+
+const port = 8080;
+
+app.use("/customers", customersRouter);
+
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
+});
